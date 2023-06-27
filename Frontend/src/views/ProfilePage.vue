3e7fa4a8093aa64b2fd1612e714dc24e15d88300
Timothée FOUGERON <78433383+Izerty@users.noreply.github.com>
2023-06-27 10:28:34 +0200

    link front/Back - loans dashboard
    + fix pb backend

--- Frontend/src/views/ProfilePage.vue
+++ Frontend/src/views/ProfilePage.vue
@@ -36,12 +36,17 @@
                 <label for="username" class="block text-sm text-black">Total amount of your loan</label>
                 <input type="text" placeholder="Amount" class="block mt-2 w-full placeholder-gray-400/70 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40" />
               </div>
+              
             </div>
+            
 
 
           </div>
+          
         </div>
+        
       </main>
+      
     </div>
   </section>
 
